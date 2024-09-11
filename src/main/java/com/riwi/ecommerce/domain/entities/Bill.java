534a448906068eb52d26cb7c56ea5eccdf3e7ba7
Diego Ramirez <diegomejiasobsu@gmail.com>
2024-09-11 14:22:42 -0500

    add the relations between the entities

--- src/main/java/com/riwi/ecommerce/domain/entities/Bill.java
+++ src/main/java/com/riwi/ecommerce/domain/entities/Bill.java
@@ -1,4 +1,40 @@
 package com.riwi.ecommerce.domain.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Bill {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDate date;
+
+    private Double total;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToMany
+    @JoinTable(
+            name = "factura_producto",
+            joinColumns = @JoinColumn(name = "factura_id"),
+            inverseJoinColumns = @JoinColumn(name = "producto_id")
+    )
+    private List<Product> productos = new ArrayList<>();
+
 }
--- src/main/java/com/riwi/ecommerce/domain/entities/Product.java
+++ src/main/java/com/riwi/ecommerce/domain/entities/Product.java
@@ -1,4 +1,25 @@
 package com.riwi.ecommerce.domain.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+
+    private Double price;
+
+    @ManyToMany(mappedBy = "productos")
+    private List<Bill> bills = new ArrayList<>();
 }
