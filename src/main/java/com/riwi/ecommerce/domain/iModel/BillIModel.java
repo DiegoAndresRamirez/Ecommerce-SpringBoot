99c003d89cf1fb0b91e8e1d9f922b5497340a91a
Diego Ramirez <diegomejiasobsu@gmail.com>
2024-09-11 19:19:43 -0500

    create the bill service

--- src/main/java/com/riwi/ecommerce/domain/iModel/BillIModel.java
+++ src/main/java/com/riwi/ecommerce/domain/iModel/BillIModel.java
@@ -6,9 +6,9 @@ import java.util.List;
 import java.util.Optional;
 
 public interface BillIModel {
-    Bill createBill(Bill Bill);
+    Bill createBill(Bill bill);
     List<Bill> getAllBills();
-    Optional<Bill> getBillById(Long BillId);
-    Bill updateBill(Bill Bill);
-    void deleteBill(Long BillId);
+    Optional<Bill> getBillById(Long billId);
+    Bill updateBill(Bill bill);
+    void deleteBill(Long billId);
 }
